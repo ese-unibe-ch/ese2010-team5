9a7963375ab01441f5da3373627d971ab45b9b07
patrick <patrick.kraus@students.unibe.ch>
2010-10-27 00:22:11 +0200

    stable version of editUserProfile

--- qa/app/controllers/Users.java
+++ qa/app/controllers/Users.java
@@ -8,16 +8,22 @@ import utils.QaDB;
 
 public class Users extends Auth{
 
-	public static void edit(long id) {
-		Logger.debug("Edit user: " + id);
-
-		User u = QaDB.findUserById(id);
-		System.out.println(id);
-
+	public static void edit(String username) {
+		Logger.debug("Edit user: " + username);
+		User u = QaDB.findUserByName(username);
 		if(u == null){
-			error("could not find user with id: "+id);
+			error("could not find user: \""+username+"\"");
 			return;
 		}	
 		render(u);
+	}
+	
+	public static void updateProfile(long id, String name, String email){
+		User user = QaDB.findUserById(id);
+		if (user == null)
+			flash("error", "could not find user with id: "+id);
+		user.setUsername(name);
+		user.setEmail(email);
+		redirect("/users/edit/"+user.getName());
 	}
 }
--- qa/app/models/User.java
+++ qa/app/models/User.java
@@ -9,7 +9,9 @@ import edu.emory.mathcs.backport.java.util.LinkedList;
 
 public class User {
 
-	private String username;	
+	private String username;
+	private String email;
+	private String dateOfBirth;
 	private long id;	  
 	private static long idCounter = 1;	 
 	
@@ -23,6 +25,30 @@ public class User {
 		return this.username;
 	}
 	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
 	public long getId(){
 		return id;
 	}
