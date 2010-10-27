d7d9aebbcd039720f96439beb796cfd758a61f3f
patrick <patrick.kraus@students.unibe.ch>
2010-10-27 19:03:53 +0200

    edit user profile implemented. some issues with changing username

--- qa/app/controllers/Users.java
+++ qa/app/controllers/Users.java
@@ -1,5 +1,10 @@
 package controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import play.*;
 import play.mvc.Controller;
 
@@ -18,12 +23,29 @@ public class Users extends Auth{
 		render(u);
 	}
 	
-	public static void updateProfile(long id, String name, String email){
+	// TODO: need to change Auth in order to be able to change username. --> use user-ID
+	public static void updateProfile(long id, String name, String email, String birthDate, String homepage){
 		User user = QaDB.findUserById(id);
 		if (user == null)
 			flash("error", "could not find user with id: "+id);
-		user.setUsername(name);
+		//QaDB.removeUser(user);
+		//user.setUsername(name);
+		//QaDB.addUser(user);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN);
+		Date date = new Date();
+		try {
+			date = sdf.parse(birthDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (date != null){
+			user.setBirthDate(date);
+		}
 		user.setEmail(email);
+		user.setHomepage(homepage);
+		Logger.debug("updated user profile of: " + user.getName());
+		flash.put("info","Profile information of " + user.getName() + " changed");
 		redirect("/users/edit/"+user.getName());
 	}
 }
