631b72dea5df90f0fa72d9ef48dc9fd82b171b95
Daniel Moser <daniel.moser@students.unibe.ch>
2010-11-03 09:35:47 +0100

    First 2 Classes with javadoc

--- qa/app/models/MockUser.java
+++ qa/app/models/MockUser.java
@@ -1,9 +1,18 @@
 package models;
 
+/**
+ * The Class MockUser used for tests.
+ */
 public class MockUser implements IUser {
 
+	/** The username. */
 	private String username;
 
+	/**
+	 * Instantiates a new mock user.
+	 *
+	 * @param username the username
+	 */
 	public MockUser(String username) {
 		this.username = new String(username);
 	}
