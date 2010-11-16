9cd6839e76146bff791e6adaad7f8356bccab6ef
Daniel Moser <daniel.moser@students.unibe.ch>
2010-11-16 09:03:58 +0100

    made some more tests, coverage about 75%

--- qa/test/team5customtests/TagTest.java
+++ qa/test/team5customtests/TagTest.java
@@ -13,14 +13,30 @@ import org.junit.*;
 public class TagTest extends UnitTest{
 	Tag tag;
 	//Sorry had to do it without the interface (would have to change the whole application)
-	Question quest;
-	IUser use;
+	Question quest,quest1;
+	IUser use,use1;
 
 	@Before
 	public void setUp() {
 		use = new MockUser("Hans");
+		use1 = new MockUser("Joggeli");
 		quest = new Question(use,"JUnitTestQuestion");
-		tag = new Tag("junit",quest);		
+		quest = new Question(use1,"JUnitTestQuestion1");
+		tag = new Tag("junit",quest);	
+	}
+	
+	@Test
+	public void shouldReturnRelatedQuestionsCount(){
+		tag.registerQuestion(quest1);
+		assertEquals(2,tag.count());	
+	}
+	
+	@Test
+	public void shouldHaveOneQuestionLessAfterUnregister(){
+		tag.registerQuestion(quest1);
+		assertEquals(2,tag.count());
+		tag.unregisterQuestion(quest1);
+		assertEquals(1,tag.count());
 	}
 	
 	@Test
