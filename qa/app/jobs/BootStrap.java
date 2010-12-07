38a4c4ea8338cf1761cbab78bab61f67013779e1
Cedric Reginster <cederigo@gmail.com>
2010-12-08 00:30:32 +0100

    - produce less questions

--- qa/app/jobs/BootStrap.java
+++ qa/app/jobs/BootStrap.java
@@ -43,8 +43,8 @@ public class BootStrap extends Job {
     	
     	log.info("fill Model with test-data");
     	
-    	int users = 5;
-    	int questionsPerUser = 5;
+    	int users = 1;
+    	int questionsPerUser = 2;
     	
     	//create some users
     	for(int i=1;  i <= users ; i++){
